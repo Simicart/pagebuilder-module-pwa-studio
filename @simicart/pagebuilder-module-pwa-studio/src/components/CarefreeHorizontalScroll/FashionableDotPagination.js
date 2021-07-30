f8dfd80df0e09bf837b80dbb4c0d5de993804bcc
lanapone <lanapone@lanapones-MBP.lan>
2021-07-30 17:07:14 +0700

    change pagination icon to svg

--- @simicart/pagebuilder-module-pwa-studio/src/components/CarefreeHorizontalScroll/FashionableDotPagination.js
+++ @simicart/pagebuilder-module-pwa-studio/src/components/CarefreeHorizontalScroll/FashionableDotPagination.js
@@ -11,9 +11,9 @@ export const SimpleDot = (props) => {
 
 export const DotWithOrbital = (props) => {
 	const { className = '', ...rest } = props || {};
-	const newClass = generateClass(defaultClasses, 'zmdi zmdi-dot-circle', 'orbital-dot', className);
+	const newClass = generateClass(defaultClasses, '', 'orbital-dot', className);
 
-	return <i className={newClass} {...rest} />;
+    return <img className={newClass} src={require('./icon/dot-circle.svg')} alt={''} {...rest} />;
 };
 
 export const HorizontalBar = (props) => {
