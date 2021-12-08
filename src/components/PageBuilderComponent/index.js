9bc6ef781b9f90e6d04210aafefb463793464686
Cody Nguyen <cody@simicart.com>
2021-12-08 16:47:59 +0700

    global style appliance

--- src/components/PageBuilderComponent/index.js
+++ src/components/PageBuilderComponent/index.js
@@ -27,7 +27,22 @@ const HOPageBuilderComponent = props => {
             }
         };
     }, []);
-    return <PageBuilderComponent {...props} />;
+    
+    return (
+        <div
+            style={{
+                fontFamily: `"Poppins", Helvetica, Arial, sans-serif`,
+                fontSize: '14px',
+                lineHeight: 1.5
+            }}
+        >
+            <style>
+                @import
+                url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+            </style>
+            <PageBuilderComponent {...props} />
+        </div>
+    );
 };
 
 export default HOPageBuilderComponent;
